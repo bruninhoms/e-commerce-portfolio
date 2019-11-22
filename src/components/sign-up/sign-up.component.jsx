4facdf8a125fda9fe64b690051d81bd63d1dcbac
Bruno Mattias <brunodemattias@gmail.com>
2019-11-22 10:47:46 -0300

    Bugs de cadastro de e-mail, e login corrigidos, sem warnings

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component.jsx';
 import CustomButton from '../custom-button/custom-button.component.jsx';
 
-import { auth, createUserProfileDocument } from 'firebase';
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils.js';
 
 import './sign-up.styles.scss';
 
