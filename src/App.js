309de14bce592bd74f98f725ebc215d06643259b
Bruno Mattias <brunodemattias@gmail.com>
2019-11-24 16:21:00 -0300

    checkout page, half working, dropdown toggle when checkout, new elector for total

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import CheckoutPage from './pages/checkout/checkout.component.jsx';
+
 import Header from './components/header/header.component.jsx';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
+
 import { setCurrentUser } from './redux/user/user.actions.js';
+import { selectCurrentUser } from './redux/user/user.selectors.js';
 
 class App extends React.Component {
   unsubscribeFromAuth = null
@@ -44,16 +50,23 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route 
+          exact 
+          path='/signin' 
+          render={() => 
+          this.props.currentUser ? 
+          (<Redirect to='/' />) : 
+          (<SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
